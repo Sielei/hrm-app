f1ed0aada73bb42d594d7d6e16e6426c8994d037
Sielei Herman <hsielei@gmail.com>
2023-11-14 14:49:25 +0300

    chore: initialize a spring boot starter project

---
+++ src/main/java/com/hrmapp/HrmAppApplication.java
@@ -0,0 +1,13 @@
+package com.hrmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrmapp/HrmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
