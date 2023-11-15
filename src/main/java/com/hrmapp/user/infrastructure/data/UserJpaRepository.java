2dfcb78a0ca52fbd3cf8edbec9189bcefae66e6c
Sielei Herman <hsielei@gmail.com>
2023-11-15 19:29:55 +0300

    created a JPA data repository for users.

---
+++ src/main/java/com/hrmapp/user/infrastructure/data/UserJpaRepository.java
@@ -0,0 +1,10 @@
+package com.hrmapp.user.infrastructure.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
+}
