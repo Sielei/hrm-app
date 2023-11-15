ea8944c055a0c603b1f0e4cb18a38d61647abf5a
Sielei Herman <hsielei@gmail.com>
2023-11-15 19:29:07 +0300

    created rest controllers for users and password policies.

---
+++ src/main/java/com/hrmapp/user/application/controller/PasswordPolicyController.java
@@ -0,0 +1,9 @@
+package com.hrmapp.user.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/password-policies")
+public class PasswordPolicyController {
+}
