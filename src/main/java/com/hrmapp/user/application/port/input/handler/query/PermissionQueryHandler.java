d99e7c227e8c08ba3ccd2f066c6081844bfdf9be
Sielei Herman <hsielei@gmail.com>
2023-11-15 19:22:52 +0300

    created query handler classes for user domain entities. This classes will handle all query operations related to their respective entities.

---
+++ src/main/java/com/hrmapp/user/application/port/input/handler/query/PermissionQueryHandler.java
@@ -0,0 +1,9 @@
+package com.hrmapp.user.application.port.input.handler.query;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class PermissionQueryHandler {
+}
