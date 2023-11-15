4d40cbe757b2f00736edf70e7dd7ecfa50c05f44
Sielei Herman <hsielei@gmail.com>
2023-11-15 19:19:56 +0300

    created command handler classes for user entities.

---
+++ src/main/java/com/hrmapp/user/application/port/input/handler/command/PermissionCommandHandler.java
@@ -0,0 +1,9 @@
+package com.hrmapp.user.application.port.input.handler.command;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class PermissionCommandHandler {
+}
