fceb35d06f661ec8c43bbf718e3b7a3eac2d61cc
Sielei Herman <hsielei@gmail.com>
2023-11-16 13:42:00 +0300

    created an email service that user java mail sender to send emails.

---
+++ src/main/java/com/hrmapp/email/service/EmailService.java
@@ -0,0 +1,42 @@
+package com.hrmapp.email.service;
+
+import com.hrmapp.email.entity.Email;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine springTemplateEngine;
+    private static final Logger log = LoggerFactory.getLogger(SLF4JLogger.class);
+
+    public EmailService(JavaMailSender javaMailSender, SpringTemplateEngine springTemplateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.springTemplateEngine = springTemplateEngine;
+    }
+
+    public void sendHtmlEmail(Email email) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+        Context context = new Context();
+        context.setVariables(email.properties());
+        helper.setFrom(email.from());
+        helper.setTo(email.to());
+        helper.setSubject(email.subject());
+        String html = springTemplateEngine.process(email.template(), context);
+        helper.setText(html, true);
+
+        log.info("Sending email: {} with html body: {}", email, html);
+        javaMailSender.send(message);
+    }
+}
