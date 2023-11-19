e09cfb0fb0744177e6e9bd4e542d832ff33c57ec
Sielei Herman <hsielei@gmail.com>
2023-11-19 20:47:35 +0300

    created dto for update password command

---
+++ src/main/java/com/hrmapp/user/application/dto/command/UpdatePasswordCommand.java
@@ -0,0 +1,36 @@
+package com.hrmapp.user.application.dto.command;
+
+import java.util.UUID;
+
+public record UpdatePasswordCommand(UUID userId, String currentPassword, String newPassword) {
+    public static Builder builder() {
+        return new Builder();
+    }
+    public static final class Builder {
+        private UUID userId;
+        private String currentPassword;
+        private String newPassword;
+
+        private Builder() {
+        }
+
+        public Builder userId(UUID val) {
+            userId = val;
+            return this;
+        }
+
+        public Builder currentPassword(String val) {
+            currentPassword = val;
+            return this;
+        }
+
+        public Builder newPassword(String val) {
+            newPassword = val;
+            return this;
+        }
+
+        public UpdatePasswordCommand build() {
+            return new UpdatePasswordCommand(userId, currentPassword, newPassword);
+        }
+    }
+}
