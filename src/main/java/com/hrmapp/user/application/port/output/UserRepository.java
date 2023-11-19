c10b6348dc215d284711f14307673d50ac9779a8
Sielei Herman <hsielei@gmail.com>
2023-11-19 20:30:37 +0300

    added methods to create, find user byId and find user by email

--- src/main/java/com/hrmapp/user/application/port/output/UserRepository.java
+++ src/main/java/com/hrmapp/user/application/port/output/UserRepository.java
@@ -4,7 +4,14 @@ import com.hrmapp.common.application.dto.UserDto;
 import com.hrmapp.user.domain.entity.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmailAddress(String email);
+
+    Optional<User> findById(UUID userId);
+
+    User save(User user);
 }
