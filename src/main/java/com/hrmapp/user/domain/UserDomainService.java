3878b9fae68a24dfb9157abdebebd88e485e86eb
Sielei Herman <hsielei@gmail.com>
2023-11-19 21:10:46 +0300

    added domain methods for session

--- src/main/java/com/hrmapp/user/domain/UserDomainService.java
+++ src/main/java/com/hrmapp/user/domain/UserDomainService.java
@@ -1,4 +1,25 @@
 package com.hrmapp.user.domain;
 
+import com.hrmapp.user.domain.entity.PasswordReset;
+import com.hrmapp.user.domain.entity.Session;
+import com.hrmapp.user.domain.entity.User;
+
 public class UserDomainService {
+    public void createPasswordReset(PasswordReset passwordReset) {
+        passwordReset.createPasswordReset();
+    }
+
+    public void updateUserPassword(User user, String password) {
+        user.updatePassword(password);
+    }
+
+    public Session createSession(Session session) {
+        session.createSession();
+        return session;
+    }
+
+    public Session terminateSession(Session session) {
+        session.terminateSession();
+        return session;
+    }
 }
