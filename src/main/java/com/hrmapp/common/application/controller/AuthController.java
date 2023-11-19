60e2d8f0af2a31206218e8b9c82bdd17e2e2843f
Sielei Herman <hsielei@gmail.com>
2023-11-19 20:46:01 +0300

    added endpoint for password reset and logout

--- src/main/java/com/hrmapp/common/application/controller/AuthController.java
+++ src/main/java/com/hrmapp/common/application/controller/AuthController.java
@@ -1,14 +1,17 @@
 package com.hrmapp.common.application.controller;
 
+import com.hrmapp.common.application.dto.GenericResponse;
 import com.hrmapp.common.application.dto.request.LoginRequest;
+import com.hrmapp.common.application.dto.request.PasswordResetRequest;
+import com.hrmapp.common.application.dto.request.ResetPasswordRequest;
 import com.hrmapp.common.application.port.input.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -22,5 +25,37 @@ public class AuthController {
     @PostMapping("/login")
     ResponseEntity<?> login(@RequestBody @Validated LoginRequest loginRequest, HttpServletRequest httpServletRequest){
         return authenticationService.login(loginRequest, httpServletRequest);
+    }
+    @PostMapping("/get-password-reset-token")
+    ResponseEntity<GenericResponse> getPasswordResetToken(@RequestBody PasswordResetRequest passwordResetRequest){
+        authenticationService.generatePasswordResetToken(passwordResetRequest.emailAddress());
+        return ResponseEntity.ok().body(GenericResponse.builder()
+                        .success(true)
+                        .message("A password reset link has been sent to " + passwordResetRequest.emailAddress())
+                .build());
+    }
+    @PostMapping("/reset-password")
+    public ResponseEntity<GenericResponse> resetPassword(@RequestBody ResetPasswordRequest resetPasswordRequest){
+        authenticationService.resetPasswordWithResetToken(resetPasswordRequest);
+        return new ResponseEntity<>(GenericResponse.builder()
+                .success(true)
+                .message("Password reset successfully! Proceed to login page to login with your new password!")
+                .build(), HttpStatus.OK);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<GenericResponse> logout(HttpServletRequest request){
+        //TODO get user token and invalidate it
+        //authenticationService.logout(request);
+        return new ResponseEntity<>(GenericResponse.builder()
+                .success(true)
+                .message("Successfully logged out!")
+                .build(), HttpStatus.OK);
+    }
+
+    @PostMapping("/refreshToken")
+    public ResponseEntity<?> getRefreshToken(@RequestAttribute("userId") UUID userId, HttpServletRequest request){
+        var refreshToken = authenticationService.getRefreshToken(userId, request);
+        return new ResponseEntity<>(refreshToken, HttpStatus.OK);
     }
 }
