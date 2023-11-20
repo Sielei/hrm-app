044b849b37e36e10ee21c51975be95bc591be934
Sielei Herman <hsielei@gmail.com>
2023-11-20 17:41:56 +0300

    implement find role by id method.

--- src/main/java/com/hrmapp/user/application/port/output/RoleRepository.java
+++ src/main/java/com/hrmapp/user/application/port/output/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.hrmapp.user.application.port.output;
 
+import com.hrmapp.user.domain.entity.Role;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface RoleRepository {
+    Optional<Role> findById(UUID roleId);
 }
--- src/main/java/com/hrmapp/user/infrastructure/adapter/RoleDbAdapter.java
+++ src/main/java/com/hrmapp/user/infrastructure/adapter/RoleDbAdapter.java
@@ -1,14 +1,27 @@
 package com.hrmapp.user.infrastructure.adapter;
 
 import com.hrmapp.user.application.port.output.RoleRepository;
+import com.hrmapp.user.domain.entity.Role;
+import com.hrmapp.user.infrastructure.UserDataMapper;
 import com.hrmapp.user.infrastructure.data.RoleJpaRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class RoleDbAdapter implements RoleRepository {
     private final RoleJpaRepository roleJpaRepository;
+    private final UserDataMapper userDataMapper;
 
-    public RoleDbAdapter(RoleJpaRepository roleJpaRepository) {
+    public RoleDbAdapter(RoleJpaRepository roleJpaRepository, UserDataMapper userDataMapper) {
         this.roleJpaRepository = roleJpaRepository;
+        this.userDataMapper = userDataMapper;
+    }
+
+    @Override
+    public Optional<Role> findById(UUID roleId) {
+        return roleJpaRepository.findById(roleId)
+                .map(userDataMapper::mapRoleJpaEntityToRole);
     }
 }
