c1617f551dae49f5c1b56eca598a61e33e6607d1
Sielei Herman <hsielei@gmail.com>
2023-11-24 19:23:06 +0300

    create method stubs to be implemented later

--- src/test/java/com/hrmapp/user/infrastructure/adapter/FakePermissionDB.java
+++ src/test/java/com/hrmapp/user/infrastructure/adapter/FakePermissionDB.java
@@ -1,4 +1,22 @@
 package com.hrmapp.user.infrastructure.adapter;
 
-public class FakePermissionDB {
+import com.hrmapp.user.application.dto.PermissionDto;
+import com.hrmapp.user.application.port.output.PermissionRepository;
+import com.hrmapp.user.domain.entity.Permission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class FakePermissionDB implements PermissionRepository {
+    @Override
+    public Optional<Permission> findById(UUID permissionId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Page<PermissionDto> findPermissions(Pageable pageable) {
+        return null;
+    }
 }
--- src/test/java/com/hrmapp/user/infrastructure/adapter/FakeRoleDB.java
+++ src/test/java/com/hrmapp/user/infrastructure/adapter/FakeRoleDB.java
@@ -1,4 +1,32 @@
 package com.hrmapp.user.infrastructure.adapter;
 
-public class FakeRoleDB {
+import com.hrmapp.user.application.dto.RoleDto;
+import com.hrmapp.user.application.port.output.RoleRepository;
+import com.hrmapp.user.domain.entity.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class FakeRoleDB implements RoleRepository {
+    @Override
+    public Optional<Role> findById(UUID roleId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Role save(Role role) {
+        return null;
+    }
+
+    @Override
+    public Page<RoleDto> findRoles(PageRequest pageable) {
+        return null;
+    }
+
+    @Override
+    public void deleteRole(Role role) {
+
+    }
 }
